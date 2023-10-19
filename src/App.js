96aa3d5035b80fcffda27182a8d142f0058fcd44
hiraeth282 <77190943+hiraeth282@users.noreply.github.com>
2023-10-18 22:05:21 -0500

    Began work on HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <div className="container">
+        <div className="card mt-5 mb-5">
+          <div className="card-body">
+           <form>
+             <div className="row">
+               <div className="col-sm-9">
+                 <input type="search" placeholder="Enter a city..." className="form-control"/>
+                </div>
+                <div className="col-sm-3">
+                  <input type="submit" value="Search" className="btn"/>
+                </div>
+              </div>                
+            </form>
+           </div>
+          </div>
+        </div>
+      </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
@@ -8,6 +9,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <footer>
+      This project was coded by {""} 
+      <a href="https://verdant-mermaid-8e9627.netlify.app/" target="_blank" rel="noreferrer">Evelyn Ewald</a>
+      {""} and is {""}
+    <a href="https://github.com/hiraeth282/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+    </footer>
   </React.StrictMode>
 );
 
