af612ff6f5f491faf7ef472c241fdabe8786a3e9
hiraeth282 <77190943+hiraeth282@users.noreply.github.com>
2023-10-20 18:49:37 -0500

    Integrated api data into html

--- src/Weather.js
+++ src/Weather.js
@@ -9,19 +9,15 @@ export default function Weather() {
     console.log(response.data.main)
     setWeatherData({
       ready: true,
-      temperature: response.data.main.temp,
+      temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
       icon: response.data.weather[0].icon,
-      wind: response.data.wind.speed,
-      city: response.data.name
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      date: "Saturday, 11:00"
   })
   }
-  
-  const apiKey = "9cb72bec958f8fb02391985ed7b219d2";
-  let city = "Tokyo";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
   
   if (weatherData.ready) {
     return (
@@ -41,28 +37,28 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1 className="mt-3">{city}</h1>
+        <h1 className="mt-3">{weatherData.city}</h1>
         <ul>
           <li>
             Saturday, 11:00
           </li>
-          <li>
-            Rainy
+          <li className="text-capitalize">
+            {weatherData.description}
           </li>
         </ul>
         <div className="row">
           <div className="col-sm-6">
             <img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="rain icon" className="mb-4"></img>
-            <span className="current-temperature">10</span>
+            <span className="current-temperature">{weatherData.temperature}</span>
             <span className="unit">°C</span>
           </div>
           <div className="col-sm-6">
             <ul>
               <li>
-                Humidity: 20%
+                Humidity: {weatherData.humidity}%
               </li>
               <li>
-                Wind: 2 kph
+                Wind: {weatherData.wind}kph
               </li>
             </ul>
           </div>
@@ -70,6 +66,10 @@ export default function Weather() {
       </div>
     );
   } else {
-    return "Loading...";
+      const apiKey = "9cb72bec958f8fb02391985ed7b219d2";
+      let city = "Tokyo";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&q=${city}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+  
   }
 }
